6215fcdf755e09d607589645b4b40ab8b7fdf7aa
alessandraciccone <alessandraciccone@icloud.com>
2025-10-24 10:42:13 +0200

    add security

---
+++ src/main/java/com/example/Organizzazione_Eventi/security/JWTFilter.java
@@ -0,0 +1,63 @@
+package com.example.Organizzazione_Eventi.security;
+
+
+import entities.User;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+public class JWTFilter extends OncePerRequestFilter {
+    private final JWWTTools jwtTools;
+    private final UserService userService;
+
+    public JWTFilter(JWWTTools jwtTools, UserService userService) {
+        this.jwtTools = jwtTools;
+        this.userService = userService;
+
+        @Override
+        protected void doFilterInternal (HttpServletRequest request, HttpServletResponse response, FilterChain
+        filterChain)throw ServletException,
+        IOException {
+
+            //recuperiamo tokern
+            String authHeader = request.hetHeader("Authorization");
+            if (authHeader == null || !authHeader.startsWith("Bearer")) {
+                throw new UnauthorizedException("token mancante");
+            }
+            //estriamo token
+            String accessToken = authHeader.replace("Bearer", "");
+            //verifichiamo validità token
+            jwtTools.verifyToken(accessToken);
+            //prendiamo id utente del token
+
+            UUID userId = jwtTools.extractIdFromToken(accessToken);
+
+            //prendiamo utente dal db
+
+            User found = userService.findById(userId);
+
+            // creo authentication
+            Authentication authentication = new UsernamePasswordAuthenticationToken(found, null, found.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            filterChain.doFilter(request, respon00se00);
+        }
+
+
+        @Override
+        protected boolean shouldNotFilter (httpServletRequest request)throw ServletException {
+            return new AntiPatgMatcher().match("/auth/**", request.getServletPath());
+        }
+    }
+}
---
+++ src/main/java/com/example/Organizzazione_Eventi/security/JWWTTools.java
@@ -0,0 +1,57 @@
+package com.example.Organizzazione_Eventi.security;
+
+import entities.User;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class JWWTTools {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    //facciamo durare il token 7 giorni
+
+    private static final long EXPIRATION_TIME= 1000*60*60*24*7;
+
+
+    //creo jwt
+
+    public String createToken(User user){
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+ EXPIRATION_TIME))
+                .subject(String.valueOf(user.getId()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+
+    }
+
+    //verifico validità token
+
+    public void verifyToken(String accessToken){
+        try{
+            Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parse(accessToken);
+        } catch(Exception ex){
+            throw  new UnauthorizedException("token non valido o sacduto"
+            );
+        }
+    }
+
+    //estraggo uiid utente dal subject token
+
+    public UUID extractIdFromToken(String accessToken){
+        return UUID.fromString(Jwts.parser()
+                .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .build()
+                .parseSignedClaims(accessToken)
+                .getPayload()
+                .getSubject());
+    }
+
+}
